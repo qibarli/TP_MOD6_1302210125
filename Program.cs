173e51788f090dadc96cde31d9e8f7f0232b28a1
qibarli <barliant.rifqi20@gmail.com>
2023-03-26 23:33:36 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TP_MOD6_1302210125
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            SayaTubeVideo saya = new SayaTubeVideo(" Tutorial Design By Contract - Mochamad Rifqi Barliant ");
+
+            saya.IncreasePlayCount(0);
+
+            saya.PrintVideoDetails();
+
+            Console.WriteLine();
+
+            SayaTubeVideo saya1 = new SayaTubeVideo(" Mochamad Rifqi Barliant");
+
+            saya1.IncreasePlayCount(1000000000);
+
+            saya1.PrintVideoDetails();
+
+        }
+    }
+}
